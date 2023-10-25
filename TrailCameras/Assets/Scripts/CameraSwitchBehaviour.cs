211387650fc09b53817764e38d33ff2db7d51ddb
caden <cpete.est2014@gmail.com>
2023-10-25 09:57:59 -0600

    limit camera drawcalls

--- TrailCameras/Assets/Scripts/CameraSwitchBehaviour.cs
+++ TrailCameras/Assets/Scripts/CameraSwitchBehaviour.cs
@@ -9,27 +9,46 @@ public class CameraSwitchBehaviour : MonoBehaviour
     public UnityEvent CameraSwitch;
 
     public Camera camera1;
-    public Camera camera2; 
+    public Camera camera2;
     public Camera camera3;
-  
-    private int currentCamera = 0;
 
-    public void UpdateCamera() 
+    private int currentCamera = 0; 
+
+    void Start()
     {
+        camera2.gameObject.SetActive(false);
+        camera3.gameObject.SetActive(false);
+    }
+
+    void UpdateCamera() 
+    {
+
         currentCamera++;
-        if(currentCamera > 2)
-        {
+        if(currentCamera > 2) {
             currentCamera = 0;  
         }
-      
+
         switchCamera();
-        
+
     }
-  
-    void switchCamera() {
-        camera1.gameObject.SetActive(currentCamera == 0);
-        camera2.gameObject.SetActive(currentCamera == 1); 
-        camera3.gameObject.SetActive(currentCamera == 2);
+
+    void switchCamera()
+    {
+        
+        camera1.gameObject.SetActive(false);
+        camera2.gameObject.SetActive(false);
+        camera3.gameObject.SetActive(false);
+        
+        if(currentCamera == 0) {
+            camera1.gameObject.SetActive(true);
+        }
+        else if(currentCamera == 1) {
+            camera2.gameObject.SetActive(true);
+        }
+        else if(currentCamera == 2) {
+            camera3.gameObject.SetActive(true);
+        }
+
     }
 
 }
